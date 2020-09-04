88576a97351f77d357a63ba591c1a53c06dfde4e
Stefan Dimitrov <stefan@Stefans-MacBook-Air.local>
2020-09-04 07:41:26 +0700

    Initial commit.

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+export default {
+    output: {
+        format: 'umd',
+        name: 'uhst'
+    },
+    external: [
+      // put some third party libraries here
+    ],
+    onwarn: ( warning ) => {
+        const skip_codes = [
+            'THIS_IS_UNDEFINED',
+            'MISSING_GLOBAL_NAME'
+        ];
+        if ( skip_codes.indexOf(warning.code) != -1 ) return;
+        console.error(warning);
+    }
+  };
